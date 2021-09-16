06fa0f6f1475d1dcfa8726170816b690c20ea2ca
JumpingDino <35410520+JumpingDino@users.noreply.github.com>
2021-09-16 15:10:42 -0300

    LEC and data
    LEC and Downloaded Datasets

---
+++ downloadDataset.sh
@@ -0,0 +1,38 @@
+# NELAGT2020 
+
+# labels
+wget https://dataverse.harvard.edu/api/access/datafile/4366864
+
+#covid
+wget https://dataverse.harvard.edu/api/access/datafile/4417503
+
+# elections
+wget https://dataverse.harvard.edu/api/access/datafile/4417504
+
+#all
+wget https://dataverse.harvard.edu/api/access/datafile/4417502
+
+# readme
+wget https://dataverse.harvard.edu/api/access/datafile/4366874
+
+
+
+# NELAGT2019
+
+# labels
+wget https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/O7FWPO/QV9KD9
+
+# content
+wget https://dataverse.harvard.edu/api/access/datafile/4417513
+
+# readme 1
+wget https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/O7FWPO/RWEGLH
+
+# readme 2
+wget https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/O7FWPO/LRL0KY
+
+
+
+
+
+
